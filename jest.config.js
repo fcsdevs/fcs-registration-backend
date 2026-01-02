829bf54370fce1717cdbd70d178c2e08665e9be7
Ashsamson <ashaolusamson85@gmail.com>
2026-01-02 11:56:48 +0100

    workflow fixes

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 export default {
   testEnvironment: 'node',
-  roots: ['<rootDir>/src', '<rootDir>/tests'],
+  roots: ['<rootDir>/src'],
   testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'],
   collectCoverageFrom: [
     'src/**/*.js',
